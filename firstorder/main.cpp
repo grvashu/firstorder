445bcbee43b4ecac83783dafdddb21e706553685
Gaurav Singh <grv@mathscapes.xyz>
2019-01-31 23:27:03 +0530

    Initial Commit

---
+++ firstorder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstorder
+//
+//  Created by Gaurav Singh on 31/01/19.
+//  Copyright © 2019 Mathscapes Research. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
